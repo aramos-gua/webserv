584890c2dcdb4f4b740c3339f7dbae2ad28701f6
aramos-gua <alejandro.ramos.gua@gmail.com>
2026-06-05 13:01:52 +0200

    started response

---
+++ src/HttpResponse.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpRequest.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <contact@aramos.dev>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/30 12:26:17 by aramos            #+#    #+#             */
+/*   Updated: 2026/05/30 12:31:56 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HTTPRESPONSE_HPP
+# define HTTPRESPONSE_HPP
+
+# include <string>
+# include <map>
+
+struct HttpResponse
+{
+	int			statusCode;
+	std::string	version;
+	std::string	description;
+
+	std::map<std::string, std::string>	headers;
+
+	std::string	body;
+}
+
+#endif
