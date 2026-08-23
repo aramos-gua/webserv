aea0c80a170780a71ad8af1b5103e1205383d6e5
aramos-gua <alejandro.ramos.gua@gmail.com>
2026-08-23 21:53:15 +0200

    added a sanitizer for http injection

---
+++ src/HttpResponseBuilder.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpResponseBuilder.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <contact@aramos.dev>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/23 21:43:18 by aramos            #+#    #+#             */
+/*   Updated: 2026/08/23 21:43:21 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HttpResponseBuilder.hpp"
+#include <sstream>
+
+std::string	HttpResponseBuilder::sanitizeHeaderValue(const std::string &s)
+{
+	std::string	out;
+	for (size_t i = 0; i < s.size(); ++i)
+	{
+		if (s[i] != '\r' && s[i] != '\n')
+			out += s[i];
+	}
+	return (out);
+}
+
+std::string	HttpResponseBuilder::build(const HttpResponse &res)
+{
+	std::ostringstream	out;
+	out << res.version << " " << res.statusCode << " " << sanitizeHeaderValue(res.description) << "\r\n";
+	std::map<std::string, std::string>::const_iterator	it;
+	for (it = res.headers.begin(); it != res.headers.end(); ++it)
+	{
+		if (it->first == "Content-Length" || it->first == "content-length")
+			continue ;
+		out << it->first << ": " << sanitizeHeaderValue(it->second) << "\r\n";
+	}
+	bool	hasConnection = res.headers.count("Connection") || res.headers.count("connection");
+	if (!hasConnection)
+		out << "Connection: close\r\n";
+	out << "Content-Length: " << res.body.size() << "\r\n";
+	out << "\r\n";
+	out << res.body;
+	return (out.str());
+}
--- src/HttpResponseBuilder.hpp
+++ src/HttpResponseBuilder.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   RequestParser.hpp                                  :+:      :+:    :+:   */
+/*   HttpResponseBuilder.hpp                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aramos <contact@aramos.dev>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 12:33:06 by aramos            #+#    #+#             */
-/*   Updated: 2026/05/30 12:35:24 by aramos           ###   ########.fr       */
+/*   Updated: 2026/08/23 21:37:25 by aramos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,14 @@
 # define HTTPRESPONSEBUILDER_HPP
 
 # include "HttpResponse.hpp"
-# include <sstream>
 
 class	HttpResponseBuilder
 {
-	public:
-		static std::string	build(const HttpResponse &res)
-		{
-			std::ostringstream	out;
-			out << res.version << " " << res.statusCode << " " << res.description << "\r\n";
-			std::map<std::string, std::string>::const_iterator	it;
-			for (it = res.headers.begin(); it != res.headers.end(); ++it)
-			{
-				out << it->first << ": " << it->second << "\r\n";
-			}
-			out << "Content-Length: " << res.body.size() << "\r\n";
-			out << "\r\n";
-			out << res.body;
-			return (out.str());
-		}
+	private :
+		static std::string	sanitizeHeaderValue(const std::string &s);
+
+	public :
+		static std::string	build(const HttpResponse &res);
 };
 
 # endif
